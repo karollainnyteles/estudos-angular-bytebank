1a43f6249ce9e6f40e69a1dcd232d1183a0998ac
Karollainny Teles <karollainnyteles@hotmail.com>
2021-10-05 18:46:00 -0300

    Commit initial

---
+++ src/app/nova-transferencia/nova-transnferencia.component.ts
@@ -0,0 +1,24 @@
+import { Component, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
+  valor: number;
+  destino: number;
+  transferir() {
+    console.log('Nova transferencia solicitada');
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
+}

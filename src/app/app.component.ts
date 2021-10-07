2aef9f2b050ec3f5f5c0b64d06da9537ce66c587
Karollainny Teles <karollainnyteles@hotmail.com>
2021-10-07 12:39:22 -0300

    Integrando os componentes com a API e trabalhando com Rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,4 @@ export class AppComponent {
 
   }
 
-
-  transferir($event){
-    this.service.adicionar($event);
-
-
-  }
 }
--- src/app/nova-transferencia/nova-transnferencia.component.ts
+++ src/app/nova-transferencia/nova-transnferencia.component.ts
@@ -1,3 +1,5 @@
+import { Transferencia } from './../models/transferencia.model';
+import { TransferenciaService } from './../services/transferencia.service';
 import { Component, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 
@@ -10,11 +12,19 @@ export class NovaTransferenciaComponent {
   @Output() aoTransferir = new EventEmitter<any>();
   valor: number;
   destino: number;
+
+  constructor(private service : TransferenciaService){}
+
   transferir() {
     console.log('Nova transferencia solicitada');
-    const valorEmitir = { valor: this.valor, destino: this.destino };
-    this.aoTransferir.emit(valorEmitir);
-    this.limparCampos();
+    const valorEmitir : Transferencia = { valor: this.valor, destino: this.destino };
+
+    this.service.adicionar(valorEmitir).subscribe(resultado => {
+      console.log(resultado);
+      this.limparCampos();
+    },
+    error => console.error(error)
+    );
   }
 
   limparCampos() {
